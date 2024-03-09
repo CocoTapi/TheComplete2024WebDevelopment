9afef0325f38b721ce977828d2aba7635090713d
Coco Tapi <shioriavino@gmail.com>
2024-03-09 18:15:11 -0500

    added a delete request

--- section_30/jokeAPI/index.js
+++ section_30/jokeAPI/index.js
@@ -79,17 +79,32 @@ app.patch("/jokes/:id", (req, res) => {
     jokeType: req.body.type || existingJoke.jokeType,
   }
 
- const searchedIndex = jokes.findIndex((joke) => joke.id === id);
- jokes[searchedIndex] = updatedJoke;
+  const searchedIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[searchedIndex] = updatedJoke;
 
- console.log(jokes[searchedIndex]);
+  console.log(jokes[searchedIndex]);
 
- res.json(updatedJoke);
+  res.json(updatedJoke);
 })
 
 //7. DELETE Specific joke
+app.delete("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const searchedIndex = jokes.findIndex((joke) => joke.id === id);
+
+  if (searchedIndex > -1) {
+    jokes.splice(searchedIndex, 1);
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Joke with id: ${id} not found. No jokes were deleted.`})
+  }
+})
+
 
 //8. DELETE All jokes
+
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
